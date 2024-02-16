1bf1808525b0eef5769b189b1db8209ad8c784de
youaresogay22 <youaresogayee@gmail.com>
2024-02-16 12:43:59 +0900

    project3(Stereotype annotations) done

--- src/main/java/com/nhnacademy/edu/springframework/messagesender/config/MainConfig.java
+++ src/main/java/com/nhnacademy/edu/springframework/messagesender/config/MainConfig.java
@@ -1,12 +1,12 @@
 package com.nhnacademy.edu.springframework.messagesender.config;
 
-import com.nhn.dooray.client.DoorayHookSender;
-import org.springframework.context.annotation.*;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
 @ComponentScan(basePackages = {"com.nhnacademy.edu.springframework.messagesender"})
 @EnableAspectJAutoProxy
 public class MainConfig {
- 
+
 }
