4ff78cdecac19cd757f270b5dc4a11656db0cc3a
youaresogay22 <youaresogayee@gmail.com>
2024-02-16 12:43:42 +0900

    project2(Stereotype annotations) done

--- src/main/java/com/nhnacademy/edu/springframework/messagesender/impl/DoorayMessageSender.java
+++ src/main/java/com/nhnacademy/edu/springframework/messagesender/impl/DoorayMessageSender.java
@@ -6,13 +6,20 @@ import com.nhnacademy.edu.springframework.messagesender.MessageSender;
 import com.nhnacademy.edu.springframework.messagesender.annotation.Dooray;
 import com.nhnacademy.edu.springframework.messagesender.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 @Component
 @Primary
 public class DoorayMessageSender implements MessageSender {
     DoorayHookSender doorayHookSender;
+
+    @Bean
+    public static DoorayHookSender doorayHookSender() {
+        return new DoorayHookSender(new RestTemplate(), "https://hook.dooray.com/services/3204376758577275363/3738651962004364986/HByzZBRuRu69-EUTsWR0tg");
+    }
 
     @Autowired
     public DoorayMessageSender(DoorayHookSender doorayHookSender) {
